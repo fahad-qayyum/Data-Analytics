b9530c4f34817cfd8aabe4f17908168c51b9ceed
Hakim Hani <hakeem@my.yorku.ca>
2019-10-11 21:19:29 -0400

    working part C

--- network.py
+++ network.py
@@ -1,30 +1,33 @@
 import pandas as pd
 import argparse
 import csv; import math; import sys; 
-#from nltk.tokenize import word_tokenize
 from string import punctuation
 from operator import itemgetter
 import re as re
 csv.field_size_limit(sys.maxsize)
 
 def main():
+    # contains the "id" as the key and "list of friends" as values
     all_friends_dict = {}
-    name_mapper = {}
     file = sys.argv[1]
-    with open(file, newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            name_mapper[row['user_id']] = row['name']
-            all_friends_dict[row['user_id']] = row['friends'].split(', ')
+    with open(file, 'w') as file_object:       
+        with open(file, newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                if row['friends'] != "None": 
+                    all_friends_dict[row['user_id']] = row['friends'].split(', ')
 
-
-
-    for item in all_friends_dict:
-        for friend in all_friends_dict[item]:
-            print(name_mapper[item], name_mapper[friend])
-
-
-
+        for key, value in all_friends_dict.items():
+            for friend in value:
+                if friend in all_friends_dict.keys():
+                    if key in all_friends_dict[friend]:
+                        all_friends_dict[key].remove(friend)
+                    elif not key in all_friends_dict[friend]:
+                        file_object.write(str(key + "-->" +  friend + "\n")) 
+                        # print(key, "-->", friend)    
+                else:
+                    file_object.write(str(key +  "-->"+ friend + "\n")) 
+                # print(key, "-->", friend)
 
 if __name__ == "__main__":
-  main()
+    main()
